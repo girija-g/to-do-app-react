cc215315815a5d31118fe7b98b4e914ed88c6803
Girija Govindaraju <girijagovindaraju94@gmail.com>
2021-01-17 22:12:02 +0530

    removing firebase config details

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyABZNZDdWzTOjw0KSVlLH356cr6Y7Q0Dj0",
-  authDomain: "to-do-app-b3c50.firebaseapp.com",
-  projectId: "to-do-app-b3c50",
-  storageBucket: "to-do-app-b3c50.appspot.com",
-  messagingSenderId: "435599752369",
-  appId: "1:435599752369:web:1a9d3587137d305996b690",
-  measurementId: "G-6QQD95M0E2"
+  /*firebase config*/
 })
 
 const db = firebaseApp.firestore()
